7acfb057a7a89542ffb736da19f3ff069fd41c70
Antoine Tian <antoine186@hotmail.com>
2021-02-12 17:36:33 +0000

    Played with user input

--- script.js
+++ script.js
@@ -6,3 +6,6 @@ document.querySelector('.message').textContent = 'Correct Number! 😍';
 
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
